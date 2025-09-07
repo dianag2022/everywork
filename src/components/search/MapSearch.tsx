05e63c8dc69c03401ec8c41db124879ea2e48f50
dianag2022 <dianagomez151019@gmail.com>
2025-09-07 01:15:09 -0500

    adjust any type

--- src/components/search/MapSearch.tsx
+++ src/components/search/MapSearch.tsx
@@ -16,7 +16,13 @@ interface MapSearchProps {
 }
 
 // Fix Leaflet default marker icons
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+interface LeafletIconDefault extends L.Icon.Default {
+  _getIconUrl?: () => string;
+}
+
+const defaultIcon = L.Icon.Default.prototype as LeafletIconDefault;
+delete defaultIcon._getIconUrl;
+
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
