b075d80da93558b9699e6f82912f0702dc30e21f
dianag2022 <dianagomez151019@gmail.com>
2025-10-15 01:27:38 -0500

    FIX ANY

--- src/components/forms/LocationMapPicker.tsx
+++ src/components/forms/LocationMapPicker.tsx
@@ -8,8 +8,10 @@ import 'leaflet/dist/leaflet.css'
 import { ServiceLocation } from '@/types/database'
 import { MapPin } from 'lucide-react'
 
+
+
 // Fix for default marker icons in Next.js
-delete (L.Icon.Default.prototype as any)._getIconUrl
+delete (L.Icon.Default.prototype as unknown as Record<string, unknown>)._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
